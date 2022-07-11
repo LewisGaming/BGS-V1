2af4d11cd95022c4e01db174460faf87fd8a9c06
LewisGaming <82121387+LewisGaming@users.noreply.github.com>
2022-07-12 00:00:02 +0100

    Create Example.lua

---
+++ Example.lua
@@ -0,0 +1,5 @@
+_G.PetWebhook = "WEBHOOK HERE"
+_G.disabledRaritys = {["Common"] = true, ["Unique"] = true,["Rare"] = true, ["Epic"] = true} -- true means pets with the rarity is disabled!
+_G.disabledPets = {["Doggy"] = true} -- true means the pet WILL NOT SHOW, Leave blank like this {} for any and every pet to show!
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/LewisGaming/BGS-v1/main/Main.lua'))()
